479009587e241d187cafe90c0438043ecd87a68d
Zeeshan Rizwan <zeeshanrizwan666@gmail.com>
2023-12-04 07:49:42 -0800

    hello api

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello Backend World! with github actionns');
 });
+app.get('/api', function (req, res) {
+  res.send('Hello API ');
+});
 app.listen(3000, function () {
   console.log('Example app listening on port: 3000');
 });
